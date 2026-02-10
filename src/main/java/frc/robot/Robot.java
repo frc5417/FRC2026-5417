2d23a6cbc0c4b3c09fd56d8c8b34f85e650109ee
HamiltonFan08 <seoyoon.jeong@student.allenisd.org>
2026-02-10 17:51:15 -0600

    Added line to add camera feed on Elastic

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -122,6 +122,8 @@ public class Robot extends TimedRobot {
       SmartDashboard.putBoolean("Valid Tags Loaded", validTagsPresent);
     }
 
+    CameraServer.startAutomaticCapture();
+
     // This makes sure that the autonomous stops running when
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
