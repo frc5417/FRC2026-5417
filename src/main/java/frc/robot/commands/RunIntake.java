d6633e5889a8630cd74e393bdeca6d5950c25d3b
HamiltonFan08 <seoyoon.jeong@student.allenisd.org>
2026-03-06 14:49:13 -0600

    Programmed RunIntake

---
+++ src/main/java/frc/robot/commands/RunIntake.java
@@ -0,0 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+import frc.robot.subsystems.Intake;
+import frc.robot.Constants;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class RunIntake extends Command {
+  /** Creates a new RunIntake. */
+  private Intake m_intake;
+  private double pos;
+  private boolean terminate = false;
+
+  public RunIntake(Intake intake, double pos) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    m_intake = intake;
+    this.pos = pos;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    m_intake.setIntakeAnglePos(this.pos);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    terminate = true;
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return terminate;
+  }
+}
