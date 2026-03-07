826884c9289c0e1decf8725292720e07c8a4e9b7
Christopher Cagle <cagleclc@gmail.com>
2026-03-07 10:31:07 -0600

    Added commands to stop the climber

--- src/main/java/frc/robot/commands/TeleOp.java
+++ src/main/java/frc/robot/commands/TeleOp.java
@@ -4,6 +4,7 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Climb;
 
@@ -21,18 +22,18 @@ public class TeleOp extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    SmartDashboard.putData("Field", m_field);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-  
+    m_climber.stopClimber();
   }
 
   // Returns true when the command should end.
