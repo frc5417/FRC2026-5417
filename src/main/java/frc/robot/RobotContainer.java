1fad9836424ded1b1b896f7d3c49d87f3d4945f9
whYXZee <124375418+whYXZee@users.noreply.github.com>
2026-03-08 15:38:10 -0500

    Pathplanner drive PID tuning

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -73,7 +73,7 @@ public class RobotContainer {
                     m_robotDrive::getRobotRelativeSpeeds,
                     (speeds, feedforwards) -> m_robotDrive.drive(speeds.vxMetersPerSecond, speeds.vyMetersPerSecond,
                             speeds.omegaRadiansPerSecond, false),
-                    new PPHolonomicDriveController(new PIDConstants(5.0, 0, 0), new PIDConstants(5.0, 0, 0)),
+                    new PPHolonomicDriveController(new PIDConstants(2.5, 0, 0), new PIDConstants(5.0, 0, 0)),
                     config,
                     () -> {
                         var alliance = DriverStation.getAlliance();
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -18,6 +18,7 @@ import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -47,6 +48,7 @@ public class DriveSubsystem extends SubsystemBase {
       DriveConstants.kBackRightChassisAngularOffset);
 
   private ChassisSpeeds robotRelSpeeds = new ChassisSpeeds(0, 0, 0);
+  private Field2d field = new Field2d();
 
   // The gyro sensor
   private final Pigeon2 m_pigeon = new Pigeon2(Constants.Identification.pigeonId);
@@ -124,6 +126,9 @@ public class DriveSubsystem extends SubsystemBase {
         });
 
     SmartDashboard.putData(this);
+
+    field.setRobotPose(m_odometry.getPoseMeters());
+    SmartDashboard.putData(field);
   }
 
   /**
