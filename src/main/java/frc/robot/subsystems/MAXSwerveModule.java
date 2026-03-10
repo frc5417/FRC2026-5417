b1923fd26d2f6b59fb78c0af5ffac84af4abe422
HamiltonFan08 <seoyoon.jeong@student.allenisd.org>
2026-03-10 15:21:33 -0500

    Changing the variable name of the Swerve drive motor

--- src/main/java/frc/robot/subsystems/MAXSwerveModule.java
+++ src/main/java/frc/robot/subsystems/MAXSwerveModule.java
@@ -26,7 +26,7 @@ import frc.robot.Configs;
 import frc.robot.Constants;
 
 public class MAXSwerveModule extends SubsystemBase {
-  private final SparkFlex m_drivingSpark;
+  private final SparkFlex m_drivingFlex;
   private final SparkMax m_turningSpark;
 
   private final RelativeEncoder m_drivingEncoder;
@@ -48,10 +48,10 @@ public class MAXSwerveModule extends SubsystemBase {
    * Encoder.
    */
   public MAXSwerveModule(int drivingCANId, int turningCANId, double chassisAngularOffset) {
-    m_drivingSpark = new SparkFlex(drivingCANId, MotorType.kBrushless);
+    m_drivingFlex = new SparkFlex(drivingCANId, MotorType.kBrushless);
     m_turningSpark = new SparkMax(turningCANId, MotorType.kBrushless);
 
-    m_drivingEncoder = m_drivingSpark.getEncoder();
+    m_drivingEncoder = m_drivingFlex.getEncoder();
     m_turningEncoder = m_turningSpark.getAbsoluteEncoder();
 
     turningPID = new PIDController(Constants.ModuleConstants.angleKP, Constants.ModuleConstants.angleKI,
@@ -60,12 +60,12 @@ public class MAXSwerveModule extends SubsystemBase {
     // Apply the respective configurations to the SPARKS. Reset parameters before
     // applying the configuration to bring the SPARK to a known good state. Persist
     // the settings to the SPARK to avoid losing them on a power cycle.
-    m_drivingSpark.configure(Configs.MAXSwerveModule.drivingConfig, ResetMode.kResetSafeParameters,
+    m_drivingFlex.configure(Configs.MAXSwerveModule.drivingConfig, ResetMode.kResetSafeParameters,
         PersistMode.kPersistParameters);
     m_turningSpark.configure(Configs.MAXSwerveModule.turningConfig, ResetMode.kResetSafeParameters,
         PersistMode.kPersistParameters);
 
-    m_drivingClosedLoopController = m_drivingSpark.getClosedLoopController();
+    m_drivingClosedLoopController = m_drivingFlex.getClosedLoopController();
     m_turningClosedLoopController = m_turningSpark.getClosedLoopController();
 
     m_chassisAngularOffset = chassisAngularOffset;
